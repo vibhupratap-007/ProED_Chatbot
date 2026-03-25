76c41b85a24d03afea15e4285488b011d964d29c
Vibhu Pratap <vibhupratap007@gmail.com>
2026-03-25 14:35:20 +0530

    changes in main file

--- main.py
+++ main.py
@@ -132,3 +132,9 @@ ANSWER:"""
         answer=answer,
         sources=sources
     )
+    
+    #Setting self Port 
+    if __name__ == "__main__":
+        import uvicorn
+        port = int(os.environ.get("PORT", 8000))
+        uvicorn.run("main:app", host="0.0.0.0", port=port)
